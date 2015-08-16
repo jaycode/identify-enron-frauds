e2fcf0ddc1b95350c50387918b0c01a4b3a2aad6
jaycode <teguhwpurwanto@gmail.com>
2015-08-16 19:40:46 +0700

    chore: Init

---
+++ tools/feature_format_with_names.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+""" 
+    Similar with featureFormat plus names in its returned values
+"""
+
+
+import numpy as np
+
+def featureFormatWithNames( dictionary, features, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=False, sort_keys = False):
+    return_list = []
+
+    # Key order - first branch is for Python 3 compatibility on mini-projects,
+    # second branch is for compatibility on final project.
+    if isinstance(sort_keys, str):
+        import pickle
+        keys = pickle.load(open(sort_keys, "rb"))
+    elif sort_keys:
+        keys = sorted(dictionary.keys())
+    else:
+        keys = dictionary.keys()
+
+    final_keys = []
+    for key in keys:
+        tmp_list = []
+        for feature in features:
+            try:
+                dictionary[key][feature]
+            except KeyError:
+                print "error: key ", feature, " not present"
+                return
+            value = dictionary[key][feature]
+            if value=="NaN" and remove_NaN:
+                value = 0
+            tmp_list.append( float(value) )
+
+        # Logic for deciding whether or not to add the data point.
+        append = True
+        # exclude 'poi' class as criteria.
+        if features[0] == 'poi':
+            test_list = tmp_list[1:]
+        else:
+            test_list = tmp_list
+        ### if all features are zero and you want to remove
+        ### data points that are all zero, do that here
+        if remove_all_zeroes:
+            append = False
+            for item in test_list:
+                if item != 0 and item != "NaN":
+                    append = True
+                    break
+        ### if any features for a given data point are zero
+        ### and you want to remove data points with any zeroes,
+        ### handle that here
+        if remove_any_zeroes:
+            if 0 in test_list or "NaN" in test_list:
+                append = False
+        ### Append the data point if flagged for addition.
+        if append:
+            final_keys.append(key)
+            return_list.append( np.array(tmp_list) )
+
+    return [np.array(return_list), final_keys]
